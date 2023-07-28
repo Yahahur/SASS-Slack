6c16eb546441485765eb8979b828b7d1e38da634
Yahahur <rodmaron_recinto@dlsl.edu.ph>
2023-07-28 13:11:52 +0800

    hindi pa tapos

--- dist/script.js
+++ dist/script.js
@@ -23,17 +23,20 @@ window.addEventListener("scroll", () => {
     }
 });
 
+const closeModalHandler = ()=>{
+    modal.classList.toggle('active');
+    btnRegion.classList.toggle('active')
+}
+
+const openModalHandler = ()=>{
+    btnRegion.classList.toggle('active');
+    btnRegion.nextElementSibling.classList.toggle('active')
+}
+
 const modal = document.querySelector('.modal');
 const btnRegion = document.querySelector('.region');
 const btnClose = document.querySelector('#closeModal');
 
-console.log(btnRegion)
-btnRegion.addEventListener('click',()=>{
-    btnRegion.classList.toggle('active')
-    btnRegion.nextElementSibling.classList.toggle('active')
-})
 
-btnClose .addEventListener('click',()=>{
-    modal.classList.toggle('active');
-    btnRegion.classList.toggle('active')
-})
+btnClose.addEventListener('click',closeModalHandler)
+btnRegion.addEventListener('click',openModalHandler);
